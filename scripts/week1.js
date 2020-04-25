c29d963e6085bab062a0c4df131b062b97d75a67
izzysilv <35540604+izzysilv@users.noreply.github.com>
2020-04-25 15:45:07 -0600

    Added week 1 and 2 folders

---
+++ scripts/week1.js
@@ -0,0 +1,9 @@
+const toggleNotes = () => {
+    let x = document.getElementById("notesWeek1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  

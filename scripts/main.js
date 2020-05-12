42e7a3914807e7f91bde486640ea553a2344c25a
izzysilv <35540604+izzysilv@users.noreply.github.com>
2020-05-12 00:31:27 -0600

    Finished array cardio 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 // Function to toggle my main menu
+/*
 const toggleMenu = () => {
     document.querySelector('#navigation').classList.toggle('show');
 }
 
 document.querySelector('#menu').addEventListener('click', toggleMenu);
-
+*/
